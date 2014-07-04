e3635275512d8d78d2296c91790d97ec6e432527
trmiller <trmiller@gmail.com>
2014-07-03 22:30:24 -0400

    using incorrect express syntax

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res){
-	res.render('app/index.html');
+	res.sendfile('app/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
